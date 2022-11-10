680406b6c53e85aa5cf6278fe1955bdd9620dbb1
unknown <youssef.ojeil@hotmail.com>
2022-11-09 19:55:40 -0500

    get all posts and single post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,14 +1,6 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
 
-// route to get all post
-// router.get('/', async (req, res) => {
-//   const postData = await Post.findAll().catch((err) => {
-//     res.json(err);
-//   });
-//   const posts = postData.map((post) => post.get({ plain: true }));
-//   res.render('all-posts', { posts });
-// });
 
 router.get('/', async (req, res) => {
   try {
@@ -36,21 +28,6 @@ router.get('/', async (req, res) => {
   }
 });
 
-// route to get one post
-// router.get('/post/:id', async (req, res) => {
-//   try {
-//     const postData = await Post.findByPk(req.params.id);
-//     if (!postData) {
-//       res.status(404).json({ message: 'No Post found with this id!' });
-//       return;
-//     }
-//     const post = postData.get({ plain: true });
-//     res.render('single-post', post);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   };
-// });
-
 router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
@@ -64,6 +41,7 @@ router.get('/post/:id', async (req, res) => {
 
     const post = postData.get({ plain: true });
 
+    console.log(post)
     res.render('single-post', {
       ...post,
       logged_in: req.session.logged_in
